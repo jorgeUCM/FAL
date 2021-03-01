8fb4f0391871fb701db4d56cf41073d496444b34
jorge millan garcia <45824339+jorgeUCM@users.noreply.github.com>
2021-03-01 10:53:04 +0100

    Add files via upload

---
+++ Problema 01/Enunciado01/Source.cpp
@@ -0,0 +1,51 @@
+﻿// Nombre del alumno .....
+// Usuario del Juez ......
+
+
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+
+// Resuelve un caso de prueba, leyendo de la entrada la
+// configuración, y escribiendo la respuesta
+void resuelveCaso() {
+    int num, cont = 0, max;
+    std::cin >> num;
+    max = num;
+    while (num != 0) {
+        
+        if (num > max) {
+            max = num;
+            cont = 1;
+        }
+        else if (num == max) {
+            cont++;
+        }
+        std::cin >> num;
+    }
+    std::cout << max << " " << cont << std::endl;
+}
+
+int main() {
+    // Para la entrada por fichero.
+    // Comentar para acepta el reto
+#ifndef DOMJUDGE
+    std::ifstream in("Texto.txt");
+    auto cinbuf = std::cin.rdbuf(in.rdbuf()); //save old buf and redirect std::cin to casos.txt
+#endif 
+
+
+    int numCasos;
+    std::cin >> numCasos;
+    for (int i = 0; i < numCasos; ++i)
+        resuelveCaso();
+
+
+    // Para restablecer entrada. Comentar para acepta el reto
+#ifndef DOMJUDGE // para dejar todo como estaba al principio
+    std::cin.rdbuf(cinbuf);
+    system("PAUSE");
+#endif
+
+    return 0;
+}
